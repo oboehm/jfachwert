e1d426e8202be4c04c41fff451d9b5e47c260af3
oboehm <o.boehm@optica.de>
2019-08-27 17:42:27 +0200

    WaehrungsformatProviderSpi hinzugefügt

---
+++ src/main/java/de/jfachwert/bank/internal/WaehrungsformatProviderSpi.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2019 by Oliver Boehm
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express orimplied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * (c)reated 27.08.2019 by oboehm (ob@jfachwert.de)
+ */
+package de.jfachwert.bank.internal;
+
+import de.jfachwert.bank.GeldbetragFactory;
+import de.jfachwert.bank.GeldbetragFormatter;
+
+import javax.money.format.AmountFormatContext;
+import javax.money.format.AmountFormatQuery;
+import javax.money.format.MonetaryAmountFormat;
+import javax.money.spi.MonetaryAmountFormatProviderSpi;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Locale;
+import java.util.Set;
+
+/**
+ * Klasse WaehrungsformatProviderSpi.
+ *
+ * @author oboehm
+ * @since x.x (27.08.2019)
+ */
+public class WaehrungsformatProviderSpi implements MonetaryAmountFormatProviderSpi  {
+
+    /**
+     * Liefert eine Liste mit dem {@link GeldbetragFormatter} zurueck.
+     *
+     * @param formatQuery der {@link AmountFormatContext}, der verwendet werden
+     *                    soll
+     * @return Liste mit {@link GeldbetragFormatter} (oder leere Liste)
+     */
+    @Override
+    public Collection<MonetaryAmountFormat> getAmountFormats(AmountFormatQuery formatQuery) {
+        Collection<MonetaryAmountFormat> amountFormats = new ArrayList<>();
+        if (!formatQuery.getProviderNames().isEmpty() &&
+                !formatQuery.getProviderNames().contains(getProviderName())) {
+            return amountFormats;
+        }
+        if (!(formatQuery.getFormatName() == null || "default".equals(formatQuery.getFormatName()))) {
+            return amountFormats;
+        }
+        if (formatQuery.getMonetaryAmountFactory() instanceof GeldbetragFactory) {
+            Locale locale = formatQuery.getLocale();
+            amountFormats.add(GeldbetragFormatter.of(locale));
+        }
+        return amountFormats;
+    }
+
+    /**
+     * Gets a list with available locales for this format provider.
+     *
+     * @return list of available locales, never null.
+     */
+    @Override
+    public Set<Locale> getAvailableLocales() {
+        return null;
+    }
+
+    /**
+     * Gets a list with available format names for this format provider.
+     *
+     * @return list of available formats, never null.
+     */
+    @Override
+    public Set<String> getAvailableFormatNames() {
+        return null;
+    }
+}
---
+++ src/test/java/de/jfachwert/bank/internal/WaehrungsformatProviderSpiTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2019 by Oliver Boehm
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express orimplied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * (c)reated 27.08.2019 by oboehm (ob@jfachwert.de)
+ */
+package de.jfachwert.bank.internal;
+
+import de.jfachwert.bank.GeldbetragFactory;
+import org.javamoney.moneta.internal.FastMoneyAmountFactory;
+import org.junit.Test;
+
+import javax.money.MonetaryAmountFactory;
+import javax.money.format.AmountFormatQuery;
+import javax.money.format.AmountFormatQueryBuilder;
+import javax.money.format.MonetaryAmountFormat;
+import java.util.Collection;
+import java.util.Locale;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit-Test fuer {@link WaehrungsformatProviderSpi}-Klasse.
+ *
+ * @author oboehm
+ */
+public final class WaehrungsformatProviderSpiTest {
+
+    private final WaehrungsformatProviderSpi providerSpi = new WaehrungsformatProviderSpi();
+
+    @Test
+    public void testGetAmountFormatsGeldbetrag() {
+        Collection<MonetaryAmountFormat> formats = getMonetaryAmountFormats(new GeldbetragFactory());
+        assertEquals(1, formats.size());
+    }
+
+    @Test
+    public void testGetAmountFormatsFastMoney() {
+        Collection<MonetaryAmountFormat> formats = getMonetaryAmountFormats(new FastMoneyAmountFactory());
+        assertTrue(formats.isEmpty());
+    }
+
+    private Collection<MonetaryAmountFormat> getMonetaryAmountFormats(MonetaryAmountFactory factory) {
+        AmountFormatQueryBuilder builder = AmountFormatQueryBuilder.of("default").setLocale(Locale.GERMAN);
+        builder.setMonetaryAmountFactory(factory);
+        AmountFormatQuery query = builder.build();
+        return providerSpi.getAmountFormats(query);
+    }
+
+}
