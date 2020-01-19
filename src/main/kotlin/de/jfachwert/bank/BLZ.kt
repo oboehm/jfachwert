10c9ca03470df0cd133a1bdbc57d06ab81d7c6ea
oboehm <o.boehm@optica.de>
2020-01-19 20:00:03 +0100

    BLZ auf Kotlin umgestellt

--- src/main/kotlin/de/jfachwert/bank/BLZ.kt
+++ src/main/kotlin/de/jfachwert/bank/BLZ.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 by Oliver Boehm
+ * Copyright (c) 2017-2020 by Oliver Boehm
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,140 +15,86 @@
  *
  * (c)reated 16.03.2017 by oboehm (ob@jfachwert.de)
  */
-package de.jfachwert.bank;
+package de.jfachwert.bank
 
-import de.jfachwert.AbstractFachwert;
-import de.jfachwert.SimpleValidator;
-import de.jfachwert.math.PackedDecimal;
-import de.jfachwert.pruefung.NullValidator;
-import de.jfachwert.pruefung.NumberValidator;
-import org.apache.commons.lang3.RegExUtils;
-
-import java.util.WeakHashMap;
+import de.jfachwert.AbstractFachwert
+import de.jfachwert.SimpleValidator
+import de.jfachwert.math.PackedDecimal
+import de.jfachwert.pruefung.NullValidator
+import de.jfachwert.pruefung.NumberValidator
+import org.apache.commons.lang3.RegExUtils
+import java.util.*
 
 /**
  * Die BLZ (Bankleitzahl) ist eine eindeutige Kennziffer, die in Deutschland
  * und Oesterreich eindeutig ein Kreditinstitut identifiziert. In Deutschland
  * ist die BLZ eine 8-stellige, in Oesterreich eine 5-stellige Zahl (mit
  * Ausnahme der Oesterreichischen Nationalbank mit 3 Stellen).
- * <p>
+ *
  * Zur Reduzierung des internen Speicherverbrauchs wird die BLZ als
- * {@link PackedDecimal} abgelegt.
- * </p>
+ * [PackedDecimal] abgelegt.
  *
  * @author oboehm
  * @since 16.03.2017
  */
-public class BLZ extends AbstractFachwert<PackedDecimal, BLZ> {
-
-    private static final WeakHashMap<String, BLZ> WEAK_CACHE = new WeakHashMap<>();
-    private static final Validator VALIDATOR = new Validator();
-
-    /** Null-Konstante fuer Initialisierungen. */
-    public static final BLZ NULL = new BLZ("", new NullValidator<>());
-
-    /**
-     * Hierueber wird eine neue BLZ angelegt.
-     *
-     * @param code eine 5- oder 8-stellige Zahl
-     */
-    public BLZ(String code) {
-        this(code, VALIDATOR);
-    }
-
-    /**
-     * Hierueber wird eine neue BLZ angelegt.
-     *
-     * @param code      eine 5- oder 8-stellige Zahl
-     * @param validator fuer die Ueberpruefung
-     */
-    public BLZ(String code, SimpleValidator<PackedDecimal> validator) {
-        super(PackedDecimal.valueOf(code), validator);
-    }
+open class BLZ
+/**
+ * Hierueber wird eine neue BLZ angelegt.
+ *
+ * @param code      eine 5- oder 8-stellige Zahl
+ * @param validator fuer die Ueberpruefung
+ */
+@JvmOverloads constructor(code: String, validator: SimpleValidator<PackedDecimal>? = VALIDATOR) :
+        AbstractFachwert<PackedDecimal, BLZ>(PackedDecimal.valueOf(code), validator) {
 
     /**
      * Hierueber wird eine neue BLZ angelegt.
      *
      * @param code eine 5- oder 8-stellige Zahl
      */
-    public BLZ(int code) {
-        this(Integer.toString(code));
-    }
-
-    /**
-     * Eine BLZ darf maximal 8-stellig sein.
-     *
-     * @param blz die Bankleitzahl
-     * @return die Bankleitzahl zur Weiterverabeitung
-     */
-    public static String validate(String blz) {
-        return VALIDATOR.validate(PackedDecimal.of(blz)).toString();
-    }
-
-    /**
-     * Liefert eine BLZ zurueck.
-     *
-     * @param code maximal 8-stellige Nummer
-     * @return die BLZ
-     */
-    public static BLZ of(int code) {
-        return of(Integer.toString(code));
-    }
-
-    /**
-     * Liefert eine BLZ zurueck.
-     *
-     * @param code maximal 8-stellige Nummer
-     * @return die BLZ
-     */
-    public static BLZ of(String code) {
-        return WEAK_CACHE.computeIfAbsent(code, BLZ::new);
-    }
+    constructor(code: Int) : this(Integer.toString(code)) {}
 
     /**
      * Liefert die unformattierte BLZ.
      *
      * @return unformattierte BLZ, z.B. "64090100"
      */
-    public String getUnformatted() {
-        return this.getCode().toString();
-    }
-    
+    val unformatted: String
+        get() = code.toString()
+
     /**
      * Liefert die BLZ in 3er-Gruppen formattiert.
      *
      * @return formatierte LBZ, z.B. "640 901 00"
      */
-    public String getFormatted() {
-        String input = this.getUnformatted() + "   ";
-        StringBuilder buf = new StringBuilder();
-        for (int i = 0; i < this.getUnformatted().length(); i+= 3) {
-            buf.append(input, i, i+3);
-            buf.append(' ');
+    val formatted: String
+        get() {
+            val input = unformatted + "   "
+            val buf = StringBuilder()
+            var i = 0
+            while (i < unformatted.length) {
+                buf.append(input, i, i + 3)
+                buf.append(' ')
+                i += 3
+            }
+            return buf.toString().trim { it <= ' ' }
         }
-        return buf.toString().trim();
-    }
-
 
     /**
      * Dieser Validator ist fuer die Ueberpruefung von BLZs vorgesehen.
      *
      * @since 2.2
      */
-    public static class Validator implements SimpleValidator<PackedDecimal> {
-
-        private static final NumberValidator NUMBER_VALIDATOR = new NumberValidator(100, 99_999_999);
-
+    class Validator : SimpleValidator<PackedDecimal> {
         /**
          * Eine BLZ darf maximal 8-stellig sein.
          *
          * @param blz die Bankleitzahl
          * @return die Bankleitzahl zur Weiterverabeitung
          */
-        @Override
-        public PackedDecimal validate(PackedDecimal blz) {
-            String normalized = validate(blz.toString());
-            return PackedDecimal.of(normalized);
+        override fun validate(blz: PackedDecimal): PackedDecimal {
+            val normalized = validate(blz.toString())
+            return PackedDecimal.of(normalized)
         }
 
         /**
@@ -157,9 +103,9 @@ public class BLZ extends AbstractFachwert<PackedDecimal, BLZ> {
          * @param blz die Bankleitzahl
          * @return die Bankleitzahl zur Weiterverabeitung
          */
-        public String validate(String blz) {
-            String normalized = RegExUtils.replaceAll(blz, "\\s", "");
-            return NUMBER_VALIDATOR.validate(normalized);
+        fun validate(blz: String?): String {
+            val normalized = RegExUtils.replaceAll(blz, "\\s", "")
+            return NUMBER_VALIDATOR.validate(normalized)
         }
 
         /**
@@ -168,11 +114,55 @@ public class BLZ extends AbstractFachwert<PackedDecimal, BLZ> {
          * @param blz die Bankleitzahl
          * @return die Bankleitzahl zur Weiterverabeitung
          */
-        public int validate(int blz) {
-            validate(Integer.toString(blz));
-            return blz;
+        fun validate(blz: Int): Int {
+            validate(Integer.toString(blz))
+            return blz
         }
 
+        companion object {
+            private val NUMBER_VALIDATOR = NumberValidator(100, 99999999)
+        }
+    }
+
+    companion object {
+
+        private val WEAK_CACHE = WeakHashMap<String, BLZ>()
+        private val VALIDATOR = Validator()
+
+        /** Null-Konstante fuer Initialisierungen.  */
+        val NULL = BLZ("", NullValidator())
+
+        /**
+         * Eine BLZ darf maximal 8-stellig sein.
+         *
+         * @param blz die Bankleitzahl
+         * @return die Bankleitzahl zur Weiterverabeitung
+         */
+        fun validate(blz: String?): String {
+            return VALIDATOR.validate(PackedDecimal.of(blz)).toString()
+        }
+
+        /**
+         * Liefert eine BLZ zurueck.
+         *
+         * @param code maximal 8-stellige Nummer
+         * @return die BLZ
+         */
+        @JvmStatic
+        fun of(code: Int): BLZ {
+            return of(Integer.toString(code))
+        }
+
+        /**
+         * Liefert eine BLZ zurueck.
+         *
+         * @param code maximal 8-stellige Nummer
+         * @return die BLZ
+         */
+        @JvmStatic
+        fun of(code: String): BLZ {
+            return WEAK_CACHE.computeIfAbsent(code) { n: String -> BLZ(n) }
+        }
     }
 
 }
