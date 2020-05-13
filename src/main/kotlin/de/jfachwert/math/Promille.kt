bf738ffa20facbf2b136736f044f5043dc320e1c
oboehm <o.boehm@optica.de>
2020-05-13 08:44:19 +0200

    Promille auf Kotlin umgestellt

--- src/main/kotlin/de/jfachwert/math/Promille.kt
+++ src/main/kotlin/de/jfachwert/math/Promille.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 by Oliver Boehm
+ * Copyright (c) 2019, 2020 by Oliver Boehm
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,118 +15,131 @@
  *
  * (c)reated 02.10.2019 by oboehm (ob@jfachwert.de)
  */
-package de.jfachwert.math;
+package de.jfachwert.math
 
-import org.apache.commons.lang3.StringUtils;
-
-import java.math.BigDecimal;
-import java.util.WeakHashMap;
+import org.apache.commons.lang3.StringUtils
+import java.math.BigDecimal
+import java.util.*
 
 /**
  * Die Klasse Prozent steht fuer den Tausendsten Teil einer Zahl.
- * Sie kann wie die {@link Prozent}-Klasse fuer Berechnungen eingesetzt
+ * Sie kann wie die [Prozent]-Klasse fuer Berechnungen eingesetzt
  * werden.
  *
  * @author oboehm
  * @since 3.0 (02.10.2019)
  */
-public class Promille extends Prozent {
-
-    private static final WeakHashMap<BigDecimal, Promille> WEAK_CACHE = new WeakHashMap<>();
-    private static final char PROMILLE_ZEICHEN = '\u2030';
-
-    /** Konstante fuer "0 Promille". */
-    public static final Promille ZERO = Promille.of(BigDecimal.ZERO);
-
-    /** Konstante fuer "1 Promille". */
-    public static final Promille ONE = Promille.of(BigDecimal.ONE);
-
-    /** Konstante fuer "10 Promillle". */
-    public static final Promille TEN = Promille.of(BigDecimal.TEN);
+open class Promille : Prozent {
 
     /**
      * Legt ein Promille-Objekt an.
      *
      * @param wert Promille-Wert, z.B. "10" fuer 10 °/oo
      */
-    public Promille(String wert) {
-        this(toNumber(wert));
-    }
+    constructor(wert: String) : this(toNumber(wert)) {}
 
     /**
      * Legt ein Promille-Objekt an.
      *
      * @param wert Promille-Wert, z.B. 10 fuer 10 °/oo
      */
-    public Promille(long wert) {
-        super(wert);
-    }
+    constructor(wert: Long) : super(wert) {}
 
     /**
      * Legt ein Promille-Objekt an.
      *
      * @param wert Promille-Wert, z.B. 10 fuer 10 °/oo
      */
-    public Promille(BigDecimal wert) {
-        super(wert);
-    }
-
-    /**
-     * Die of-Methode liefert fuer dieselbe Zahl immer dasselbe Objekt zurueck.
-     * Diese Methode lohnt sich daher, wenn man immer denselben Promille-Wert
-     * erzeugen will, um die Anzahl der Objekte gering zu halten.
-     *
-     * @param wert z.B. "0.8"
-     * @return "0.8 °/oo" als Promille-Objekt
-     */
-    public static Promille of(String wert) {
-        return Promille.of(toNumber(wert));
-    }
-
-    private static BigDecimal toNumber(String s) {
-        String number = StringUtils.replaceChars(s, "°/o" + PROMILLE_ZEICHEN, "").trim();
-        return new BigDecimal(number);
-    }
-
-    /**
-     * Die of-Methode liefert fuer dieselbe Zahl immer dasselbe Objekt zurueck.
-     * Diese Methode lohnt sich daher, wenn man immer denselben Promille-Wert
-     * erzeugen will, um die Anzahl der Objekte gering zu halten.
-     *
-     * @param wert z.B. 2
-     * @return "2 °/oo" als Promille-Objekt
-     */
-    public static Promille of(long wert) {
-        return Promille.of(BigDecimal.valueOf(wert));
-    }
-
-    /**
-     * Die of-Methode liefert fuer dieselbe Zahl immer dasselbe Objekt zurueck.
-     * Diese Methode lohnt sich daher, wenn man immer denselben Promille-Wert
-     * erzeugen will, um die Anzahl der Objekte gering zu halten.
-     *
-     * @param wert z.B. 0.8
-     * @return "0.8 °/oo" als Promille-Objekt
-     */
-    public static Promille of(BigDecimal wert) {
-        return WEAK_CACHE.computeIfAbsent(wert, Promille::new);
-    }
+    constructor(wert: BigDecimal?) : super(wert) {}
 
     /**
      * Diese Methode liefert den mathematischen Wert als BigDecimal zurueck,
      * mit dem dann weitergerechnet werden kann. D.h. 1 Promille wird dann als
      * '0.001' zurueckgegeben.
      *
-     * @return die Zahl als {@link BigDecimal}
+     * @return die Zahl als [BigDecimal]
      */
-    @Override
-    public BigDecimal toBigDecimal() {
-        return getWert().divide(BigDecimal.valueOf(1000));
+    override fun toBigDecimal(): BigDecimal {
+        return wert.divide(BigDecimal.valueOf(1000))
     }
 
-    @Override
-    public String toString() {
-        return this.getWert() + Character.toString(PROMILLE_ZEICHEN);
+    override fun toShort(): Short {
+        return toBigDecimal().toShort()
+    }
+
+    override fun toByte(): Byte {
+        return toBigDecimal().toByte()
+    }
+
+    override fun toChar(): Char {
+        return toBigDecimal().toChar()
+    }
+
+    override fun toString(): String {
+        return wert.toString() + Character.toString(PROMILLE_ZEICHEN)
+    }
+
+
+
+    companion object {
+
+        private val WEAK_CACHE = WeakHashMap<BigDecimal, Promille>()
+        private const val PROMILLE_ZEICHEN = '\u2030'
+
+        /** Konstante fuer "0 Promille".  */
+        val ZERO = of(BigDecimal.ZERO)
+
+        /** Konstante fuer "1 Promille".  */
+        val ONE = of(BigDecimal.ONE)
+
+        /** Konstante fuer "10 Promillle".  */
+        @JvmField
+        val TEN = of(BigDecimal.TEN)
+
+        /**
+         * Die of-Methode liefert fuer dieselbe Zahl immer dasselbe Objekt zurueck.
+         * Diese Methode lohnt sich daher, wenn man immer denselben Promille-Wert
+         * erzeugen will, um die Anzahl der Objekte gering zu halten.
+         *
+         * @param wert z.B. "0.8"
+         * @return "0.8 °/oo" als Promille-Objekt
+         */
+        @JvmStatic
+        fun of(wert: String): Promille {
+            return of(toNumber(wert))
+        }
+
+        private fun toNumber(s: String): BigDecimal {
+            val number = StringUtils.replaceChars(s, "°/o$PROMILLE_ZEICHEN", "").trim { it <= ' ' }
+            return BigDecimal(number)
+        }
+
+        /**
+         * Die of-Methode liefert fuer dieselbe Zahl immer dasselbe Objekt zurueck.
+         * Diese Methode lohnt sich daher, wenn man immer denselben Promille-Wert
+         * erzeugen will, um die Anzahl der Objekte gering zu halten.
+         *
+         * @param wert z.B. 2
+         * @return "2 °/oo" als Promille-Objekt
+         */
+        @JvmStatic
+        fun of(wert: Long): Promille {
+            return of(BigDecimal.valueOf(wert))
+        }
+
+        /**
+         * Die of-Methode liefert fuer dieselbe Zahl immer dasselbe Objekt zurueck.
+         * Diese Methode lohnt sich daher, wenn man immer denselben Promille-Wert
+         * erzeugen will, um die Anzahl der Objekte gering zu halten.
+         *
+         * @param wert z.B. 0.8
+         * @return "0.8 °/oo" als Promille-Objekt
+         */
+        @JvmStatic
+        fun of(wert: BigDecimal): Promille {
+            return WEAK_CACHE.computeIfAbsent(wert) { w: BigDecimal -> Promille(w) }
+        }
+
     }
 
 }
