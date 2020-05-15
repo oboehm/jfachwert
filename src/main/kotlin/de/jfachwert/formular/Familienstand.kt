6b577c9663c508398bad8d2d97692a8ebaa4fc4a
Oli B <o.boehm@optica.de>
2020-05-15 08:09:11 +0200

    Familienstand nach Kotlin migriert

--- src/main/kotlin/de/jfachwert/formular/Familienstand.kt
+++ src/main/kotlin/de/jfachwert/formular/Familienstand.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 by Oliver Boehm
+ * Copyright (c) 2017-2020 by Oliver Boehm
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,86 +15,67 @@
  *
  * (c)reated 28.11.2017 by oboehm (ob@oasd.de)
  */
-package de.jfachwert.formular;
+package de.jfachwert.formular
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.StringUtils
 
 /**
  * Der Familienstand gehoert in Deutschland zu den Personenstandsdaten einer
  * Person und gibt an, ob diese ledig, verheiratet, geschieden oder verwitwet
  * ist. Der Familienstand wird im Melderegister gespeichert und dabei wie folgt
  * verschluesselt:
- * <ul>
- *     <li> LD (ledig) </li>
- *     <li> VH (verheiratet) </li>
- *     <li> VW (verwitwet) </li>
- *     <li> GS (geschieden) </li>
- *     <li> EA (Ehe aufgehoben) </li>
- *     <li> LP (in eingetragener Lebenspartnerschaft) </li>
- *     <li> LV (durch Tod aufgeloeste Lebenspartnerschaft) </li>
- *     <li> LA (aufgehobene Lebenspartnerschaft) </li>
- *     <li> LE (durch Todeserklärung aufgelöste Lebenspartnerschaft) </li>
- *     <li> NB (nicht bekannt) </li>
- * </ul>
- * <p>
+ *
+ *  *  LD (ledig)
+ *  *  VH (verheiratet)
+ *  *  VW (verwitwet)
+ *  *  GS (geschieden)
+ *  *  EA (Ehe aufgehoben)
+ *  *  LP (in eingetragener Lebenspartnerschaft)
+ *  *  LV (durch Tod aufgeloeste Lebenspartnerschaft)
+ *  *  LA (aufgehobene Lebenspartnerschaft)
+ *  *  LE (durch Todeserklärung aufgelöste Lebenspartnerschaft)
+ *  *  NB (nicht bekannt)
+ *
  * aus: Koordinierungsstelle für IT-Standards (Hrsg.): Datensatz für das
  * Meldewesen. Einheitlicher Bundes-/Laenderteil (DSMeld),
  * Stand: 1. Mai 2015, Blatt 1401
- * </p>
  *
  * @since 2.0
  */
-public enum Familienstand {
+enum class Familienstand(val schluessel: String, private val text: String) {
 
-    /** Ledig. */
+    /** Ledig.  */
     LEDIG("LD", "ledig"),
 
-    /** Verheiratet. */
+    /** Verheiratet.  */
     VERHEIRATET("VH", "verheiratet"),
 
-    /** Verheiratet. */
+    /** Verheiratet.  */
     VERWITWET("VW", "verwitwet"),
 
-    /** Geschieden - der Zustand nach verheiratet. */
+    /** Geschieden - der Zustand nach verheiratet.  */
     GESCHIEDEN("GS", "geschieden"),
 
-    /** Ehe aufgehoben. */
+    /** Ehe aufgehoben.  */
     EHE_AUFGEHOBEN("EA", "Ehe aufgehoben"),
 
-    /** In eingetragener Lebenspartnerschaft. */
+    /** In eingetragener Lebenspartnerschaft.  */
     EINGETRAGENE_LEBENSPARTNERSCHAFT("LP", "eingetragene Lebenspartnerschaft"),
 
-    /** Eheaehnliche Gemeinschaft. */
+    /** Eheaehnliche Gemeinschaft.  */
     EHEAEHNLICHE_GEMEINSCHAFT("EG", "ehe\u00e4hnliche Gemeinschaft"),
 
-    /** Lebenspartnerschaft, die durch den Tod beendet wurde. */
+    /** Lebenspartnerschaft, die durch den Tod beendet wurde.  */
     DURCH_TOD_AUFGELOESTE_LEBENSPARTNERSCHAFT("LV", "durch Tod aufgel\u00f6ste Lebenspartnerschaft"),
 
-    /** Aufgehobene Lebenspartnerschaft. */
+    /** Aufgehobene Lebenspartnerschaft.  */
     AUFGEHOBENE_LEBENSPARTNERSCHAFT("LA", "getrennt lebend"),
 
-    /** Lebenspartnerschaft, die durch eine Todeserklaerung aufgeloest wurde. */
+    /** Lebenspartnerschaft, die durch eine Todeserklaerung aufgeloest wurde.  */
     DURCH_TODESERKLAERUNG_AUFGELOESTE_LEBENSPARTNERSCHAFT("LE", "durch Todeserklärung aufgelöste Lebenspartnerschaft"),
 
-    /** Unbekannter Familienstand. */
+    /** Unbekannter Familienstand.  */
     NICHT_BEKANNT("NB", "nicht bekannt");
-
-    private final String schluessel;
-    private final String text;
-
-    Familienstand(String schluessel, String text) {
-        this.schluessel = schluessel;
-        this.text = text;
-    }
-
-    /**
-     * Liefert den zweistelligen Schluessel gemaess Datensatz des Meldewesens.
-     *
-     * @return z.B. "VW" fuer "verwittwet"
-     */
-    public String getSchluessel() {
-        return schluessel;
-    }
 
     /**
      * Als Ergebnis werden die einzelnen Elemente in normaler Schreibweise
@@ -102,43 +83,49 @@ public enum Familienstand {
      *
      * @return z.B. "ledig"
      */
-    @Override
-    public String toString() {
-        return text;
+    override fun toString(): String {
+        return text
     }
 
-    /**
-     * Liefert zu einem Schluessel den entsprechende Familienstand. Falls
-     * nichts gefunden wird, wird NICHT_BEKANNT zurueckgeliefert.
-     *
-     * @param schluessel z.B. "LE"
-     * @return Familienstand, z.B. LEDIG
-     */
-    public static Familienstand of(String schluessel) {
-        String normalized = StringUtils.trimToEmpty(schluessel);
-        if (normalized.length() == 2) {
-            return findSchluessel(normalized);
-        } else {
-            return findText(schluessel);
-        }
-    }
 
-    private static Familienstand findSchluessel(String schluessel) {
-        for (Familienstand familienstand : Familienstand.values()) {
-            if (familienstand.schluessel.equalsIgnoreCase(schluessel)) {
-                return familienstand;
+
+    companion object {
+
+        /**
+         * Liefert zu einem Schluessel den entsprechende Familienstand. Falls
+         * nichts gefunden wird, wird NICHT_BEKANNT zurueckgeliefert.
+         *
+         * @param schluessel z.B. "LE"
+         * @return Familienstand, z.B. LEDIG
+         */
+        @JvmStatic
+        fun of(schluessel: String): Familienstand {
+            val normalized = StringUtils.trimToEmpty(schluessel)
+            return if (normalized.length == 2) {
+                findSchluessel(normalized)
+            } else {
+                findText(normalized)
             }
         }
-        return NICHT_BEKANNT;
-    }
 
-    private static Familienstand findText(String text) {
-        for (Familienstand familienstand : Familienstand.values()) {
-            if (familienstand.text.equalsIgnoreCase(text)) {
-                return familienstand;
+        private fun findSchluessel(schluessel: String): Familienstand {
+            for (familienstand in values()) {
+                if (familienstand.schluessel.equals(schluessel, ignoreCase = true)) {
+                    return familienstand
+                }
             }
+            return NICHT_BEKANNT
         }
-        return NICHT_BEKANNT;
+
+        private fun findText(text: String): Familienstand {
+            for (familienstand in values()) {
+                if (familienstand.text.equals(text, ignoreCase = true)) {
+                    return familienstand
+                }
+            }
+            return NICHT_BEKANNT
+        }
+
     }
 
 }
