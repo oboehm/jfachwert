34a5c267482ade303e8cc6cc2f0d1b06602380fd
Oli B <o.oboehm@optica.de>
2022-01-28 08:41:24 +0100

    Text.convert(Charset) hinzugefügt (mit 1 Parameter)

--- src/test/java/de/jfachwert/TextTest.java
+++ src/test/java/de/jfachwert/TextTest.java
@@ -224,7 +224,7 @@ public final class TextTest extends FachwertTest {
 
     @Test
     public void testConvertToLatin1() {
-        assertEquals("B\u00c3\u00b6hm", Text.convert("B\u00f6hm", StandardCharsets.UTF_8, StandardCharsets.ISO_8859_1));
+        assertEquals("B\u00c3\u00b6hm", Text.convert("B\u00f6hm", StandardCharsets.ISO_8859_1));
     }
 
     @DisplayName("Konvertierung")
