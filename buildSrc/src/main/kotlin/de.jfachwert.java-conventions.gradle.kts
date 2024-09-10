0ea5682b100e149177110a7abf9f6ad570b28256
Oli B <o.boehm@optica.de>
2024-09-10 08:06:35 +0200

    gemeinsame Dependencies nach java-conventions verschoben

--- buildSrc/src/main/kotlin/de.jfachwert.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/de.jfachwert.java-conventions.gradle.kts
@@ -13,21 +13,14 @@ repositories {
     mavenCentral()
 }
 
-//dependencies {
-//    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
-//    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-//    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.20")
-//    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
-//    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
-//    testImplementation("org.junit.platform:junit-platform-commons:1.10.0")
-//    testImplementation("org.slf4j:slf4j-api:2.0.7")
-//    testImplementation("org.patterntesting:patterntesting-rt:2.3.0")
-//    testImplementation("org.javamoney:javamoney-tck:1.1")
-//    testImplementation("org.testng:testng:7.8.0")
-//    testImplementation("org.mutabilitydetector:MutabilityDetector:0.10.6")
-//    testImplementation("org.javamoney:moneta-bp:1.4.1")
-//    testImplementation("javax.annotation:javax.annotation-api:1.3.2")
-//}
+dependencies {
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+    testImplementation("org.junit.platform:junit-platform-commons:1.10.0")
+    testImplementation("org.slf4j:slf4j-api:2.0.7")
+    testImplementation("org.patterntesting:patterntesting-rt:2.3.0")
+}
 
 group = "de.jfachwert"
 version = "6.0.0-SNAPSHOT"
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,25 +6,10 @@ plugins {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    api("javax.money:money-api:1.1")
     api("org.apache.commons:commons-lang3:3.13.0")
     // optional dependency
     compileOnly("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
-    testImplementation("org.junit.platform:junit-platform-commons:1.10.0")
-    testImplementation("org.junit.platform:junit-platform-launcher:1.10.0")
     testImplementation("org.hamcrest:hamcrest:2.2")
-    testImplementation("org.mutabilitydetector:MutabilityDetector:0.10.6")
-    testImplementation("org.javamoney:moneta-bp:1.4.1")
-    testImplementation("org.javamoney:javamoney-tck:1.1") {
-        exclude(group = "org.javamoney", module = "moneta")
-        exclude(group = "org.mutabilitydetector", module = "MutabilityDetector")
-    }
-    testImplementation("org.patterntesting:patterntesting-rt:2.3.0")
-    testImplementation("org.slf4j:slf4j-api:2.0.9")
 }
 
 // Define the source JAR task
