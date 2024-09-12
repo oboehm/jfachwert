1cb50bf2da32025dbd0be534c8a75050889d4dc7
Oli B <o.boehm@optica.de>
2024-09-12 20:02:29 +0200

    jfachwert-project added

---
+++ jfachwert/build.gradle.kts
@@ -0,0 +1,24 @@
+/*
+ * Diese Datei wurde ueber 'gradle init' erstellt und dann manuell nach und
+ * nach angepasst.
+ */
+
+plugins {
+    id("de.jfachwert.java-conventions")
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":bank"))
+    implementation(project(":formular"))
+    implementation(project(":math"))
+    implementation(project(":money"))
+    implementation(project(":med"))
+    implementation(project(":net"))
+    implementation(project(":post"))
+    implementation(project(":rechnung"))
+    implementation(project(":steuer"))
+    implementation(project(":zeit"))
+}
+
+description = "jFachwert"
