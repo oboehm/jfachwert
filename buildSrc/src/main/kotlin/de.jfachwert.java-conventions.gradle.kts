60cef6795cc7bd5baaa2c06bf1e4aca8727c0fcf
Oli B <boehm@javatux.de>
2025-08-24 20:03:25 +0200

    build: next snapshot

--- buildSrc/src/main/kotlin/de.jfachwert.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/de.jfachwert.java-conventions.gradle.kts
@@ -9,7 +9,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
  */
 
 group = "de.jfachwert"
-version = "6.5.0"
+version = "6.5.1-SNAPSHOT"
 
 object Meta {
     const val desc = "Implementierung einiger Fachwerte nach dem WAM-Ansatz"
