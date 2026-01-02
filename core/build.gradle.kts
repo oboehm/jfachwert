fbe9eb0fa2244456b690d0aa44ea91fcc5e5dd4d
Oli B. <o.boehm@optica.de>
2026-01-02 22:56:20 +0100

    rollback of last commit

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -10,9 +10,9 @@ subprojects {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    api("org.apache.commons:commons-lang3:3.20.0")
+    api("org.apache.commons:commons-lang3:3.17.0")
     // optional dependency
-    compileOnly("com.fasterxml.jackson.core:jackson-databind:2.20.1")
+    compileOnly("com.fasterxml.jackson.core:jackson-databind:2.19.0")
 }
 
 sourceSets {
