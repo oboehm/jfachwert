1fc0eca82904c70c40faa9171c10c08f1aa289f8
Oli B. <boehm@javatux.de>
2026-08-12 20:28:55 +0200

    build: next snapshot

--- buildSrc/src/main/kotlin/de.jfachwert.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/de.jfachwert.java-conventions.gradle.kts
@@ -12,7 +12,7 @@ import org.cyclonedx.gradle.CyclonedxDirectTask
  */
 
 group = "de.jfachwert"
-version = "6.8.1"
+version = "6.8.2-SNAPSHOT"
 
 object Meta {
     const val desc = "Implementierung einiger Fachwerte nach dem WAM-Ansatz"
